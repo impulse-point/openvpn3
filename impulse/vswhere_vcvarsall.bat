f0ec21af58320abb85c150066a8ce707cc15308b
Jeffrey Price <jprice@impulse.com>
2018-02-28 13:13:13 -0500

    helper script for locating vcvarsall

---
+++ impulse/vswhere_vcvarsall.bat
@@ -0,0 +1,15 @@
+@echo off
+
+echo output we're parsing to locate vcvarsall.bat:
+vswhere -latest -requires Microsoft.Component.MSBuild
+
+for /f "usebackq tokens=1* delims=: " %%i in (`vswhere -latest -requires Microsoft.Component.MSBuild`) do (
+  if /i "%%i"=="installationPath" set InstallDir=%%j
+)
+
+set vcvarsall_path=%InstallDir%\VC\Auxiliary\Build\vcvarsall.bat
+echo vcvarsall.bat should be located at: %vcvarsall_path%
+
+if exist "%vcvarsall_path%" (
+  "%vcvarsall_path%" %*
+)
